9a9384d947ac3e638134e9e0b10b1fc93f31aa8b
Nikitavolzhin <volzhin.nik@mail.ru>
2024-07-10 15:42:10 +0200

    bug fixing

--- model/retrain.py
+++ model/retrain.py
@@ -1,6 +1,6 @@
 from model_func import preprocess, retrain, model_save, model_load
 
-model = model_load("model.pkl")
-X_train, X_test, y_train, y_test = preprocess("data/elementary.csv")
+model = model_load("model/model.pkl")
+X_train, X_test, y_train, y_test = preprocess("model/data/elementary.csv")
 model = retrain(model, X_train, y_train)
-model_save(model, "model.pkl")
+model_save(model, "model/model.pkl")
