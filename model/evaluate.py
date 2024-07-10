cae89b27b0f6a5e2919456b80b0ea3e0695c407a
Nikitavolzhin <volzhin.nik@mail.ru>
2024-07-10 15:30:47 +0200

    bug fix

--- model/evaluate.py
+++ model/evaluate.py
@@ -1,7 +1,7 @@
 from model_func import preprocess, model_load, evaluate
 
 def test_performace():
-    model = model_load("trained_model/model.pkl")
+    model = model_load("model.pkl")
     X_train, X_test, y_train, y_test = preprocess("data/trigo.csv")
     mse = evaluate(model, X_test)
     print("Mean Square Error of the model is {}".format(mse))
