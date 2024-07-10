65dc463907972613749e86f359608497c3e4b903
Nikitavolzhin <volzhin.nik@mail.ru>
2024-07-10 15:46:27 +0200

    bug fixing

--- model/evaluate.py
+++ model/evaluate.py
@@ -3,6 +3,6 @@ from model_func import preprocess, model_load, evaluate
 def test_performace():
     model = model_load("model/model.pkl")
     X_train, X_test, y_train, y_test = preprocess("model/data/elementary.csv")
-    mse = evaluate(model, X_test)
+    mse = evaluate(model, X_test, y_test)
     print("Mean Square Error of the model is {}".format(mse))
     assert mse > 0.01
