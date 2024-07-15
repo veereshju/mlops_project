922da808d38a8bdf5997a7b6d688f92b683b6be3
Nikitavolzhin <volzhin.nik@mail.ru>
2024-07-15 22:12:34 +0200

    some debug modifications

--- app/train.py
+++ app/train.py
@@ -1,7 +1,8 @@
-from model_func import preprocess_split, preprocess_scale, train, model_save
+from model_func import preprocess_split, preprocess_scale, train, model_save, model_load
 
 X_train, X_test, y_train, y_test = preprocess_split("model/data/trigo.csv")
 X_train = preprocess_scale(X_train)
 X_test = preprocess_scale(X_test)
 model = train(X_train, y_train)
 model_save(model, "model/model.pkl")
+model = model_load("model/model.pkl")
