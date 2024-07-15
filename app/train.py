e21d4b2c7e1876f6b8aaae8a49b3f01270be1c48
Nikitavolzhin <volzhin.nik@mail.ru>
2024-07-15 22:09:25 +0200

    fix bugs in train py file

--- app/train.py
+++ app/train.py
@@ -1,4 +1,4 @@
-from model_func import preprocess_split, preprocess_scale, train, model_save, model_load
+from model_func import preprocess_split, preprocess_scale, train, model_save
 
 X_train, X_test, y_train, y_test = preprocess_split("model/data/trigo.csv")
 X_train = preprocess_scale(X_train)
