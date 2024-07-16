89188f71a0800aaa37a2f1d46a33dea77fe30be3
Nikitavolzhin <volzhin.nik@mail.ru>
2024-07-16 09:22:53 +0200

    modifications in pipelines

--- app/train.py
+++ app/train.py
@@ -1,7 +1,6 @@
 from model_func import preprocess_split, preprocess_scale, train, model_save, model_load
-import pickle
 
-X_train, X_test, y_train, y_test = preprocess_split("model/data/trigo.csv")
+X_train, X_test, y_train, y_test = preprocess_split("model/data/elementary.csv")
 X_train = preprocess_scale(X_train)
 X_test = preprocess_scale(X_test)
 model = train(X_train, y_train)
